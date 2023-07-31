60f09c642e23589899444f128be8d70fedf5b052
Neo Sekhukhune <sekhukhune056@gmail.com>
2023-07-31 12:12:19 +0200

    Optimized list solution

--- section6-ArrayProblems/diagonal_sum.py
+++ section6-ArrayProblems/diagonal_sum.py
@@ -6,7 +6,5 @@
 def diagonal_sum(matrix):
     diagonalSum = 0
     for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            if i == j:
-                diagonalSum += matrix[i][j]
+        diagonalSum = matrix[i][i]
     return diagonalSum
