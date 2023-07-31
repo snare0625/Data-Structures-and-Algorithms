2083d6211e19e73207771b1f0f419d1a073c066a
Neo Sekhukhune <sekhukhune056@gmail.com>
2023-07-31 10:06:50 +0200

    Added Arrays.py

---
+++ section5-project-1/Array.py
@@ -0,0 +1,17 @@
+# Calcuate Average Temperature
+
+numDays = int(input("How many day's temperature"))
+total = 0
+for i in range(numDays):
+    nextDay = int(input(f"Day {i+1} 's high temp:"))
+    total += nextDay
+
+avg = round(total / numDays, 2)
+print("\nAverage = " + str(avg))
+
+above = 0
+for i in temp:
+    if i > avg:
+        above += 1
+
+print(f"{above} day(s) above average")
