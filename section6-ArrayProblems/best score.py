9f69f1a0b695164b29b77151bdb922c643dfd734
Neo Sekhukhune <sekhukhune056@gmail.com>
2023-08-01 22:39:21 +0200

    Added a second solution for best score

--- section6-ArrayProblems/best score.py
+++ section6-ArrayProblems/best score.py
@@ -5,9 +5,23 @@ Given a list, write a function to get first, second best scores from the list.
 List may contain duplicates.
 '''
 
+# first solution
 def first_second(scores):
     n = len(scores)
     scores.sort()
     first_score = scores[n - 1]
     second_score = scores[n - 2]
     return (first_score, second_score)
+
+# second solution
+def first_second_2(scores):
+    max1, max2 = float('-inf'), float('-inf')
+
+    for num in scores:
+        if num > max1:
+            max2 = max1
+            max1 = num
+        elif num > max2 and num != max1:
+            max2 = num
+    
+    return max1, max2
