d77fd0d54ff50ea5a1643f2db80eccac0914cd2b
Neo Sekhukhune <sekhukhune056@gmail.com>
2023-08-02 05:36:41 +0200

    Added permutation solution

---
+++ section6-ArrayProblems/Permutation.py
@@ -0,0 +1,14 @@
+'''
+Permutation - Check if two functions are permutaions of each other
+'''
+
+def permutation(list1, list2):
+    if len(list1) != len(list2):
+        return False
+    list1.sort()
+    list2.sort()
+    if list1 == list2:
+        return True
+    else:
+        return False
+    
