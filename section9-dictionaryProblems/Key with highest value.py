52bb421030594a90eff459617959759151daf1b3
Neo Sekhukhune <sekhukhune056@gmail.com>
2023-08-05 20:47:54 +0200

    Added Key with Highest value solution

---
+++ section9-dictionaryProblems/Key with highest value.py
@@ -0,0 +1,14 @@
+'''
+Key with the Highest Value
+Define a function which takes a dictionary as a parameter and returns the key with the highest value in a dictionary.
+'''
+
+def max_value_key(my_dict):
+    max_value = float('-inf')
+    max_key = ''
+
+    for key in my_dict:
+        if my_dict[key] > max_value:
+            max_key = key
+            max_value = my_dict[max_key]
+    return max_key
