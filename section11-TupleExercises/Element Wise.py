fffd69ac5e9388c150a491061519ad8ea5caae2a
Neo Sekhukhune <sekhukhune056@gmail.com>
2023-08-11 13:00:28 +0200

    Added ElementWise Sum Solution (Improved)

--- section11-TupleExercises/Element Wise.py
+++ section11-TupleExercises/Element Wise.py
@@ -8,3 +8,15 @@ def tuple_elementwise_sum(tuple1, tuple2):
     for i in range(len(tuple1)):
         output_tuple = (tuple1[i] + tuple2[i], )
     return output_tuple
+
+# Improved Solution 1
+def tuple_elementwise_sum2(tuple1, tuple2):
+    return tuple(map(sum, zip(tuple1, tuple2)))
+
+# Improved Solution 2
+def tuple_elementwise_sum3(tuple1, tuple2):
+    if len(tuple1) != len(tuple2):
+        raise("Input tuples must have the same length.")
+    
+    result = tuple(a + b for a, b in zip(t1, t2))
+    return result
