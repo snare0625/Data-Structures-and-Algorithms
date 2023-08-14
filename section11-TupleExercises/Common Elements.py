83e6757531b1845c8c3c5bdd0dd089765b004c14
Neo Sekhukhune <sekhukhune056@gmail.com>
2023-08-14 09:30:05 +0200

    Added Common Elements.py Solution.py

---
+++ section11-TupleExercises/Common Elements.py
@@ -0,0 +1,12 @@
+'''
+Common Elements
+Write a function that takes two tuples and returns a tuple containing the common elements of the input tuples.
+'''
+
+def common_elements(tuple1, tuple2):
+    output_tuple = ()
+    for i in range(len(tuple1)):
+        if tuple1[i] in tuple2:
+            output_tuple = output_tuple + (tuple1[i], )
+    
+    return output_tuple
