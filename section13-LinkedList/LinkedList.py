abc59b1b55409abad567a7a9c4d190d74d1e354f
Neo Sekhukhune <sekhukhune056@gmail.com>
2023-08-14 13:02:04 +0200

    Added a special LinkedList print Function

--- section13-LinkedList/LinkedList.py
+++ section13-LinkedList/LinkedList.py
@@ -9,6 +9,16 @@ class LinkedList:
         self.tail = None
         self.length = 0
 
+    def __str__(self):
+        temp_node = self.head
+        result = ''
+        while temp_node is not None:
+            result += str(temp_node.value)
+            if temp_node.next is not None:
+                result += " -> "
+            temp_node = temp_node.next
+        return result
+
     def append(self, value):
         new_node = Node(value)
         if self.head is None:
