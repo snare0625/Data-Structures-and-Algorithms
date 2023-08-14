4859507181ac262809813ce008dbda33e2e2cb56
Neo Sekhukhune <sekhukhune056@gmail.com>
2023-08-14 12:36:49 +0200

    Added LinkedList Append method

--- section13-LinkedList/LinkedList.py
+++ section13-LinkedList/LinkedList.py
@@ -9,11 +9,17 @@ class LinkedList:
         self.tail = None
         self.length = 0
 
-'''
-class LinkedList:
-    def __init__(self, value):
+    def append(self, value):
         new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
-'''
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+new_linkedlist = LinkedList()
+new_linkedlist.append(10)
+new_linkedlist.append(20)
+print(new_linkedlist)
