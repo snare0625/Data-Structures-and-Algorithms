f11b907f521456ee8edc2e4c6f85c652a8339aa9
Neo Sekhukhune <sekhukhune056@gmail.com>
2023-08-14 09:14:07 +0200

    added insert at the beginning.py solution

--- section11-TupleExercises/Insert at the beginning.py
+++ section11-TupleExercises/Insert at the beginning.py
@@ -1,2 +1,7 @@
 '''
+Insert at the Beginning
+Write a function that takes a tuple and a value, and returns a new tuple with the value inserted at the beginning of the original tuple.
 '''
+
+def insert_value_front(input_tuple, value_to_insert):
+    return (value_to_insert, ) + input_tuple
