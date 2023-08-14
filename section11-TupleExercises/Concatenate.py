4fe75988f206ca6ac6c8504fd04206caa3ddef94
Neo Sekhukhune <sekhukhune056@gmail.com>
2023-08-14 09:18:02 +0200

    Added Concatenate.py

---
+++ section11-TupleExercises/Concatenate.py
@@ -0,0 +1,6 @@
+'''Concatenate
+Write a function that takes a tuple of strings and concatenates them, separating each string with a space.
+'''
+
+def concatenate_strings(input_tuple):
+    return " ".join(input_tuple)
