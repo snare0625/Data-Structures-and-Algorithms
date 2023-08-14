c17ea98bdf4a6d3bb492d6cdaa409a338f6b1627
Neo Sekhukhune <sekhukhune056@gmail.com>
2023-08-14 12:13:29 +0200

    Created LinkedList Constructor

--- section13-LinkedList/LinkedList.py
+++ section13-LinkedList/LinkedList.py
@@ -3,5 +3,17 @@ class Node:
         self.value = value
         self.next = None
 
-new_node = Node(10)
-print(new_node)
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+'''
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+'''
