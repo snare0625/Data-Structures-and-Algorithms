1687496b3d1cc0deb0837007a646945334e334d4
Neo Sekhukhune <sekhukhune056@gmail.com>
2023-08-29 07:49:18 +0200

    Added LinkedList prepend method

--- section13-LinkedList/LinkedList.py
+++ section13-LinkedList/LinkedList.py
@@ -29,6 +29,16 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
 new_linkedlist = LinkedList()
 new_linkedlist.append(10)
 new_linkedlist.append(20)
